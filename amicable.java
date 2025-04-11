d01bf66df6fec58d4af568b186a5a513ff9a4256
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-04-11 18:53:20 +0530

    Add files via upload

---
+++ amicable.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class amicable{
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int sof_num1=isAmicableHelper(n1);
+		//System.out.println("enter:");
+		int sof_num2=isAmicableHelper(n2);
+		//System.out.println("enter:");
+		if(n1==sof_num2 && n2==sof_num1){
+			System.out.println("the numbers are amicable ");
+		} 
+		
+	}
+	
+	public static int isAmicableHelper(int n){
+		int sof_num=1;
+		int cff;
+		for(cff=2;cff*cff<=n;cff++){
+			if(n%cff==0){
+				sof_num+= (cff+(n/cff));
+			}
+		}
+		return sof_num;
+	}
+}
