209f310f38c22e2232000a04074f422aa8c5ed20
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-05-09 18:42:59 +0530

    Add files via upload

---
+++ rem_dup.java
@@ -0,0 +1,28 @@
+//Leetcode problem no:26
+//Remove Duplcates from the sorted array
+/*
+given a sorted array remove the duplicates and return the list size after removing
+Input:1|2|2|3|3|5
+output:4
+*/
+
+class rem_dup{
+	public static void main(String args[]){
+		int arr[]={1,2,2,3,3,5};
+		System.out.println(remove_duplicates(arr));
+		
+		
+	}
+	public static int remove_duplicates(int arr[]){
+		int j=0;
+	    for(int i=1;i<arr.length;i++){
+			if(arr[i]!=arr[j]){
+				j++;
+				arr[j]=arr[i];
+				
+			}
+		}
+		return j+1;
+	}
+	
+}
