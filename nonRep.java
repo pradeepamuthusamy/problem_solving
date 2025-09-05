24bd40dc115485b23a3e896bd05bbf5208403e46
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-09-05 22:14:25 +0530

    Add files via upload

---
+++ nonRep.java
@@ -0,0 +1,25 @@
+class nonRep{
+	public static void main(String[] args){
+		//String str="geeksforgeeks";
+		//String str="pradeepa";
+		//String str="meme";
+		System.out.println(findRep(str));
+	}
+		
+		static char findRep(String str){ 
+		for(int i=0;i<str.length();i++){
+			boolean check=false;
+			for(int j=0;j<str.length();j++){
+				if(i!=j && str.charAt(i)==str.charAt(j)){
+					check=true;
+					break;
+				}
+			}
+			if(!check){
+				return str.charAt(i);
+			}
+		}
+		return '$';
+	}
+}
+		
