463ac809976668359641b0c07d6c55eae0b2529d
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-09-11 19:22:40 +0530

    Add files via upload

---
+++ BinarySubString.java
@@ -0,0 +1,17 @@
+class BinarySubString{
+	public static void main(String[] args){
+		String str="000100101";
+		int len=str.length();
+		int count =0;
+		for(int i=0;i<len;i++){
+			if(str.charAt(i)=='1'){
+				for(int j=i+1;j<len;j++){
+					if(str.charAt(j)=='1'){
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
