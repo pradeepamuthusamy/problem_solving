7257a1cec0c15a85652c56a74b889fe8f95ed602
Pradeepa M <142894844+pradeepamuthusamy@users.noreply.github.com>
2025-09-14 21:58:02 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,22 @@
+
+class fibonacci{
+	public static void main(String args[]){
+		//Scanner sc new Scanner (System.in);
+		int num=10;
+		int num1=0;
+		int num2=1;
+		System.out.println("Fibonacci series:");
+
+		for(int i=0;i<num;i++){
+			System.out.println(num1);
+			int sum=num1+num2;
+			num1=num2;
+			num2=sum;
+
+		}
+				
+
+
+
+	}
+}
